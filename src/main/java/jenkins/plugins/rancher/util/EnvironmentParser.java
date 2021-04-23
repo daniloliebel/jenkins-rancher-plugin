aedd2a99c8852eac86c40bd1a1885b725b0ad49c
danilo.liebel <danilo.liebel@kmm.com.br>
2021-04-23 18:07:49 -0300

    Alteração do parse de envs para _DIV_ e _ENV_

--- src/main/java/jenkins/plugins/rancher/util/EnvironmentParser.java
+++ src/main/java/jenkins/plugins/rancher/util/EnvironmentParser.java
@@ -6,12 +6,12 @@ import java.util.Map;
 public class EnvironmentParser {
     public static Map<String, Object> parse(String environments) {
         HashMap<String, Object> map = new HashMap<>();
-        String[] fragments = environments.split(";");
+        String[] fragments = environments.split("_DIV_");
         for (String fragement : fragments) {
-            if (fragement.contains(":")) {
-                int index = fragement.indexOf(":");
+            if (fragement.contains("_ENV_")) {
+                int index = fragement.indexOf("_ENV_");
                 String env = fragement.substring(0, index);
-                String value = fragement.substring(index + 1, fragement.length());
+                String value = fragement.substring(index + 5, fragement.length());
                 map.put(env, value);
             }
         }
